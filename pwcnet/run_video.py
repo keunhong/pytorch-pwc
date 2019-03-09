45b0a4b40406ea53ea2010c0f01770b0343f193e
Keunhong Park <kpar@cs.washington.edu>
2019-03-08 21:27:51 -0800

    Automatically set output directory of not given.

--- pwcnet/run_video.py
+++ pwcnet/run_video.py
@@ -30,6 +30,10 @@ class FloWrita(multiprocessing.Process):
     def run(self):
         while True:
             batch_idx, flows = self.queue.get()
+            if batch_idx is None:
+                print("Done, exiting!")
+                break
+
             for i, flow in enumerate(flows):
                 idx = self.batch_size * batch_idx + i + 1
                 out_path = self.out_dir / f"{idx:06d}.png"
@@ -75,11 +79,16 @@ def main():
                         batch_size=args.batch_size,
                         num_workers=2)
 
-    if args.out and not args.out.exists():
-        args.out.mkdir(parents=True)
+    if args.out:
+        out_dir = args.out
+    else:
+        out_dir = args.in_path.parent / f'{args.in_path.stem}.flow'
+
+    if not out_dir.exists():
+        out_dir.mkdir(parents=True)
 
     queue = multiprocessing.Queue()
-    writer = FloWrita(args.out, args.batch_size, queue)
+    writer = FloWrita(out_dir, args.batch_size, queue)
     writer.start()
 
     tic = time.time()
@@ -88,8 +97,7 @@ def main():
         flows = model.estimate_flow(first, second, out_shape=(256, 256),
                                     preprocessed=True).cpu()
 
-        if args.out:
-            queue.put_nowait((batch_idx, flows.numpy()))
+        queue.put_nowait((batch_idx, flows.numpy()))
 
         if args.show:
             for flow in flows:
@@ -101,9 +109,11 @@ def main():
         pbar.set_description(f"fps={fps:.1f}")
         tic = time.time()
 
+    queue.put_nowait((None, None))
+
     queue.close()
     queue.join_thread()
-    writer.terminate()
+    # writer.terminate()
 
 
 if __name__ == '__main__':
